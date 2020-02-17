c17ea926c31e72288a4c5165f9d7e62ecda50902
yangself <2579987682@qq.com>
2020-02-17 21:27:39 +0800

    优化数据库代码

--- src/main/java/cn/yangself/service/GCService.java
+++ src/main/java/cn/yangself/service/GCService.java
@@ -1,28 +1,19 @@
 package cn.yangself.service;
 
+import cn.yangself.MybatisUtil;
 import cn.yangself.dao.GCDao;
 import cn.yangself.domain.ClassResult;
 import cn.yangself.domain.Result;
 import cn.yangself.domain.Student;
 import cn.yangself.domain.Temp;
-import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.List;
 
 public class GCService {
     public Temp findByNameFromTemp(String tName, String mClass) throws IOException {
-        //加载配置文件
-        InputStream in = Resources.getResourceAsStream("mybatis-config.xml");
-        //创建SqlSessionFactory对象
-        SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(in);
-        //创建SqlSession对象
-        SqlSession sqlSession = factory.openSession();
-        //获取到代理对象
+        SqlSession sqlSession = MybatisUtil.getSqlSession();
         GCDao dao = sqlSession.getMapper(GCDao.class);
         //进行查询，查询所有数据
         Temp temp = dao.findByNameFromTemp(tName, mClass);
@@ -31,12 +22,7 @@ public class GCService {
     }
 
     public Student findByNameFromStudent(String sName, String mClass) throws IOException {
-        //加载配置文件
-        InputStream in = Resources.getResourceAsStream("mybatis-config.xml");
-        //创建SqlSessionFactory对象
-        SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(in);
-        //创建SqlSession对象
-        SqlSession sqlSession = factory.openSession();
+        SqlSession sqlSession = MybatisUtil.getSqlSession();
         //获取到代理对象
         GCDao dao = sqlSession.getMapper(GCDao.class);
         //进行查询，查询所有数据
@@ -46,12 +32,7 @@ public class GCService {
     }
 
     public Temp findByNameFromTempToday(String tName, String mClass, String tDate) throws IOException {
-        //加载配置文件
-        InputStream in = Resources.getResourceAsStream("mybatis-config.xml");
-        //创建SqlSessionFactory对象
-        SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(in);
-        //创建SqlSession对象
-        SqlSession sqlSession = factory.openSession();
+        SqlSession sqlSession = MybatisUtil.getSqlSession();
         //获取到代理对象
         GCDao dao = sqlSession.getMapper(GCDao.class);
         //进行查询，查询所有数据
@@ -61,12 +42,7 @@ public class GCService {
     }
 
     public void addTemp(Temp temp) throws IOException {
-        //加载配置文件
-        InputStream in = Resources.getResourceAsStream("mybatis-config.xml");
-        //创建SqlSessionFactory对象
-        SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(in);
-        //创建SqlSession对象
-        SqlSession sqlSession = factory.openSession();
+        SqlSession sqlSession = MybatisUtil.getSqlSession();
         //获取到代理对象
         GCDao dao = sqlSession.getMapper(GCDao.class);
         dao.addTemp(temp);
@@ -76,12 +52,7 @@ public class GCService {
 
 
     public void alterTemp(Temp temp) throws IOException {
-        //加载配置文件
-        InputStream in = Resources.getResourceAsStream("mybatis-config.xml");
-        //创建SqlSessionFactory对象
-        SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(in);
-        //创建SqlSession对象
-        SqlSession sqlSession = factory.openSession();
+        SqlSession sqlSession = MybatisUtil.getSqlSession();
         //获取到代理对象
         GCDao dao = sqlSession.getMapper(GCDao.class);
         dao.alterTemp(temp);
@@ -90,12 +61,7 @@ public class GCService {
     }
 
     public List<Result> getDayTemp(String tDate) throws IOException {
-        //加载配置文件
-        InputStream in = Resources.getResourceAsStream("mybatis-config.xml");
-        //创建SqlSessionFactory对象
-        SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(in);
-        //创建SqlSession对象
-        SqlSession sqlSession = factory.openSession();
+        SqlSession sqlSession = MybatisUtil.getSqlSession();
         //获取到代理对象
         GCDao dao = sqlSession.getMapper(GCDao.class);
         //进行查询，查询所有数据
@@ -105,12 +71,7 @@ public class GCService {
     }
 
     public List<ClassResult> getDayTempList(String tDate,String mClass) throws IOException {
-        //加载配置文件
-        InputStream in = Resources.getResourceAsStream("mybatis-config.xml");
-        //创建SqlSessionFactory对象
-        SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(in);
-        //创建SqlSession对象
-        SqlSession sqlSession = factory.openSession();
+        SqlSession sqlSession = MybatisUtil.getSqlSession();
         //获取到代理对象
         GCDao dao = sqlSession.getMapper(GCDao.class);
         //进行查询，查询所有数据
